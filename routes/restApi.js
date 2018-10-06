722c476936f730563cdf0260c7a2272c86bf49f5
Dmitry Krupenya <dkrupenya@gmail.com>
2018-10-06 21:02:35 +0300

    rest api - handle errors

--- routes/restApi.js
+++ routes/restApi.js
@@ -11,7 +11,7 @@ router.get('/:collection', function (req, res, next) {
             return util.promisify(cb => collection.find().toArray(cb))();
         })
         .then(resp => res.status(200).json(resp))
-        .catch(err => res.status(400).send(err));
+        .catch(err => handleErrors(err, res));
 });
 
 router.post('/:collection', function (req, res, next) {
@@ -21,7 +21,7 @@ router.post('/:collection', function (req, res, next) {
             return util.promisify(cb => collection.insert(record, {w: 1}, cb))();
         })
         .then(resp => res.status(200).json(resp))
-        .catch(err => res.status(400).send(err));
+        .catch(err => handleErrors(err, res));
 });
 
 router.put('/:collection/:id', function (req, res, next) {
@@ -32,7 +32,17 @@ router.put('/:collection/:id', function (req, res, next) {
             return util.promisify(cb => collection.update({_id: id}, record, cb))();
         })
         .then(resp => res.status(200).json(resp))
-        .catch(err => res.status(400).send(err));
+        .catch(err => handleErrors(err, res));
+});
+
+router.delete('/:collection/:id', function (req, res, next) {
+    getCollection(req)
+        .then(collection => {
+            const id = parseInt(req.params.id);
+            return util.promisify(cb => collection.remove({_id: id}, cb))();
+        })
+        .then(resp => res.status(200).json(resp))
+        .catch(err => handleErrors(err, res));
 });
 
 router.get('/:collection/:id', function (req, res, next) {
@@ -42,8 +52,20 @@ router.get('/:collection/:id', function (req, res, next) {
             return util.promisify(cb => collection.findOne({_id: id}, cb))();
         })
         .then(resp => res.status(200).json(resp))
-        .catch(err => res.status(400).send(err));
+        .catch(err => handleErrors(err, res));
 });
+
+function handleErrors(err, res) {
+    if (err instanceof Error) {
+        console.err(err);
+        res.status(400).json({
+            name: err.name,
+            message: err.message,
+        });
+    } else {
+        res.status(400).send(err)
+    }
+}
 
 function getCollection(req) {
     const collectionName = req.params.collection;
