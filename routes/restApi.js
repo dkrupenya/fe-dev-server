a616635c89934c5b2ad4dbf39fe67173b4f246a9
Dmitry Krupenya <dkrupenya@gmail.com>
2018-10-06 19:48:14 +0300

    add simple POST and PUT rest api

--- routes/restApi.js
+++ routes/restApi.js
@@ -14,6 +14,27 @@ router.get('/:collection', function (req, res, next) {
         .catch(err => res.status(400).send(err));
 });
 
+router.post('/:collection', function (req, res, next) {
+    getCollection(req)
+        .then(collection => {
+            const record = req.body;
+            return util.promisify(cb => collection.insert(record, {w: 1}, cb))();
+        })
+        .then(resp => res.status(200).json(resp))
+        .catch(err => res.status(400).send(err));
+});
+
+router.put('/:collection/:id', function (req, res, next) {
+    getCollection(req)
+        .then(collection => {
+            const record = req.body;
+            const id = parseInt(req.params.id);
+            return util.promisify(cb => collection.update({_id: id}, record, cb))();
+        })
+        .then(resp => res.status(200).json(resp))
+        .catch(err => res.status(400).send(err));
+});
+
 router.get('/:collection/:id', function (req, res, next) {
     getCollection(req)
         .then(collection => {
