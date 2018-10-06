3253d9a0f61569b6138b7a143cda02363b98d2df
Dmitry Krupenya <dkrupenya@gmail.com>
2018-10-07 00:11:13 +0300

    add socket notifications

--- routes/restApi.js
+++ routes/restApi.js
@@ -18,7 +18,18 @@ router.post('/:collection', function (req, res, next) {
     getCollection(req)
         .then(collection => {
             const record = req.body;
+            record._id = undefined;
             return util.promisify(cb => collection.insert(record, {w: 1}, cb))();
+        })
+        // socket broadcast
+        .then(resp => {
+            notifyAll({
+                collection: req.params.collection,
+                created: (resp instanceof Array) ? resp : [resp],
+                changes: [],
+                removed: [],
+            });
+            return resp;
         })
         .then(resp => res.status(200).json(resp))
         .catch(err => handleErrors(err, res));
@@ -29,7 +40,27 @@ router.put('/:collection/:id', function (req, res, next) {
         .then(collection => {
             const record = req.body;
             const id = parseInt(req.params.id);
-            return util.promisify(cb => collection.update({_id: id}, record, cb))();
+            return util.promisify(cb => collection.update({_id: id}, record, cb))()
+                .then(numUpdated => {
+                    if (numUpdated === 0) {
+                        return Promise.reject({
+                            message: 'record was not updated',
+                        });
+                    } else {
+                        // return updated record
+                        return util.promisify(cb => collection.findOne({_id: id}, cb))();
+                    }
+                });
+        })
+        // socket broadcast
+        .then(record => {
+            notifyAll({
+                collection: req.params.collection,
+                created: [],
+                changes: [record],
+                removed: [],
+            });
+            return record;
         })
         .then(resp => res.status(200).json(resp))
         .catch(err => handleErrors(err, res));
@@ -40,6 +71,19 @@ router.delete('/:collection/:id', function (req, res, next) {
         .then(collection => {
             const id = parseInt(req.params.id);
             return util.promisify(cb => collection.remove({_id: id}, cb))();
+        })
+        // socket broadcast
+        .then(numDeleted => {
+            if (numDeleted === 1) {
+                notifyAll({
+                    collection: req.params.collection,
+                    created: [],
+                    changes: [],
+                    removed: [parseInt(req.params.id)],
+
+                });
+            }
+            return numDeleted
         })
         .then(resp => res.status(200).json(resp))
         .catch(err => handleErrors(err, res));
@@ -57,7 +101,7 @@ router.get('/:collection/:id', function (req, res, next) {
 
 function handleErrors(err, res) {
     if (err instanceof Error) {
-        console.err(err);
+        console.error(err);
         res.status(400).json({
             name: err.name,
             message: err.message,
@@ -76,6 +120,15 @@ function getCollection(req) {
     } else {
         return Promise.resolve(collections.map[collectionName]);
     }
+}
+
+function notifyAll(msg) {
+    global.appSocket.sockets.emit('changes channel', msg);
+    // const clients = global.appSocket.sockets.clients();
+    // console.log('try to send to clients ', clients.length);
+    // for ( i = 0; i < clients.length; i++ ) {
+    //     clients[i].emit('changes channel', message);
+    // }
 }
 
 module.exports = router;
